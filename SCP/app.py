5995d5e2aa11406b850868d95433d06d32b8be73
EZtargetX <114095335+EZtargetX@users.noreply.github.com>
2025-03-08 00:35:34 -0500

    Just testing (also fixed logo?)
    I wanted to clean up some stuff i was working on. i did try out some new style choices and fixed the logo though

--- SCP/app.py
+++ SCP/app.py
@@ -13,8 +13,19 @@ def connect_db():
     conn.row_factory = sqlite3.Row  # Fetch rows as dictionaries
     return conn
 
+@app.route('/test', methods=['GET']) #used for test page
+def test():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Product")  # Change 'Product' if incorrect
+    rows = cursor.fetchall()
+    data = [dict(row) for row in rows]
+    conn.close()
+    return jsonify(data)
+
+
 @app.route('/history', methods=['GET']) #purchase history
-def all_i_feel_is_Ahhhhh():
+def history():
     conn = connect_db()
     cursor = conn.cursor()
     search = request.args.get('search')
@@ -24,30 +35,23 @@ def all_i_feel_is_Ahhhhh():
     conn.close()
     return jsonify(data)
 
-@app.route('/data', methods=['GET', 'POST']) #For help desk and test "labled index
-def all_i_feel_is_pain():
+
+        
+        
+@app.route('/data', methods=['POST']) #For help desk and test labled index
+def submit_help_ticket():
     conn = connect_db()
     cursor = conn.cursor()
-
-    if request.method == 'GET':
-        cursor.execute("SELECT * FROM Product")  # Change 'Product' if incorrect
-        rows = cursor.fetchall()
-        data = [dict(row) for row in rows]
+    name = request.form.get('name')
+    email = request.form.get('email')
+    desc = request.form.get('desc')
+    try:
+        cursor.execute("INSERT INTO HelpDesk (Name, Email, Description) VALUES (?, ?, ?)", (name, email, desc))
+        conn.commit()
         conn.close()
-        return jsonify(data)
-    elif request.method == 'POST':
-        print("heck")
-        name = request.form.get('name')
-        email = request.form.get('email')
-        desc = request.form.get('desc')
-        try:
-            cursor.execute("INSERT INTO HelpDesk (Name, Email, Description) VALUES (?, ?, ?)", 
-                           (name, email, desc))
-            conn.commit()
-            conn.close()
-            return jsonify({'success': 'Ticket added successfully'}), 201
-        except sqlite3.Error as e:
-            return jsonify({'error': str(e)}), 500
+        return jsonify({'success': 'Ticket added successfully'}), 201
+    except sqlite3.Error as e:
+        return jsonify({'error': str(e)}), 500
             
 @app.route('/newlisting', methods=['POST']) # used for create new listing
 def all_i_feel_is_sad():
